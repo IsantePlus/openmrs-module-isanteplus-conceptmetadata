fd700477d7787091720ebc12d82b507d9150414d
valvijo <valeriovjoao@gmail.com>
2019-07-25 21:52:46 +0200

    Fix concepts to import

--- api/src/main/java/org/openmrs/module/isanteplusconceptmetadata/IsanteplusMetadataUtils.java
+++ api/src/main/java/org/openmrs/module/isanteplusconceptmetadata/IsanteplusMetadataUtils.java
@@ -2,19 +2,18 @@ package org.openmrs.module.isanteplusconceptmetadata;
 
 /**
  * 
- * @author Panoramica HIS
- * This class contains the metadata packages required to distribute the EMR metadata.
- * Based on eSaude metadata
+ * @author Panoramica HIS This class contains the metadata packages required to
+ *         distribute the EMR metadata. Based on eSaude metadata
  *
  */
 public class IsanteplusMetadataUtils {
-	
+
 	public static final class _PackageNames {
-		public static final String METADATA_GROUPING_CONCEPTS = "iSantePlus_custom_dictionary-3.zip";
+		public static final String METADATA_GROUPING_CONCEPTS = "iSantePlus_custom_dictionary-1.zip";
 	}
-	
+
 	public static final class _PackageUuids {
-		public static final String METADATA_GROUPING_CONCEPTS_UUID = "18f10d52-03a9-4de2-bed2-ecd62fafa995";
+		public static final String METADATA_GROUPING_CONCEPTS_UUID = "9aee55dd-37a3-4b0a-862d-0b4d1d76bbe3";
 	}
 
 }
